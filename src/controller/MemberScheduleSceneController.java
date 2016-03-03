3f6e88a864482bb3842dd6ed96c95e2b6ec4c2b4
tylerscave <tylerjones365@gmail.com>
2016-03-03 00:32:09 -0800

    Did a bunch of work on MemberSchedule

--- src/controller/MemberScheduleSceneController.java
+++ src/controller/MemberScheduleSceneController.java
@@ -1,5 +1,10 @@
 package controller;
-
+/**
+ *COPYRIGHT (C) 2016 CmpE133_7. All Rights Reserved.
+ * The controller for the member information form 
+ * Solves CmpE133 Assignment 2
+ * @author Tyler Jones,
+*/
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -20,43 +25,38 @@ import javafx.scene.control.RadioButton;
 import javafx.stage.Stage;
 import model.Context;
 import model.Member;
+import model.MemberSchedule;
 import model.PickupLocation;
-
-/**
- *COPYRIGHT (C) 2016 CmpE133_7. All Rights Reserved.
- * The controller for the member information form 
- * Solves CmpE133 Assignment 2
- * @author Tyler Jones,
-*/
 
 public class MemberScheduleSceneController implements Initializable{
 	
     private Context context;
     private Member member;
+    private MemberSchedule memberSchedule = new MemberSchedule();
     private ObservableList<PickupLocation> locations = FXCollections.observableArrayList();
     private ObservableList<String> times = FXCollections.observableArrayList();
     @FXML
     private ComboBox<PickupLocation> location;
     @FXML
-    private ComboBox monArrive;
+    private ComboBox<String> monArrive;
     @FXML
-    private ComboBox monDepart;
+    private ComboBox<String> monDepart;
     @FXML
-    private ComboBox tuesArrive;
+    private ComboBox<String> tuesArrive;
     @FXML
-    private ComboBox tuesDepart;
+    private ComboBox<String> tuesDepart;
     @FXML
-    private ComboBox wedArrive;        
+    private ComboBox<String> wedArrive;        
     @FXML
-    private ComboBox wedDepart;
+    private ComboBox<String> wedDepart;
     @FXML
-    private ComboBox thursArrive;
+    private ComboBox<String> thursArrive;
     @FXML
-    private ComboBox thursDepart;    
+    private ComboBox<String> thursDepart;    
     @FXML
-    private ComboBox friArrive;
+    private ComboBox<String> friArrive;
     @FXML
-    private ComboBox friDepart;
+    private ComboBox<String> friDepart;
     @FXML
     private RadioButton monDrive;        
     @FXML
@@ -72,6 +72,7 @@ public class MemberScheduleSceneController implements Initializable{
 	public void initialize(URL url, ResourceBundle rb) {
             context = Context.getInstance();
             member = context.getMember();
+            //memberSchedule = context.getMemberSchedule();
             
             //set up the location ComboBox
             locations.add(new PickupLocation("location1"));
@@ -82,12 +83,95 @@ public class MemberScheduleSceneController implements Initializable{
             location.setItems(locations);
             
             //setup Arrival and Departure ComboBoxes
+            times.addAll("6AM", "7AM", "8AM", "9AM", "10AM", "11AM", "12PM", "1PM", 
+            		"2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM", "10PM");
+            monArrive.setItems(times);
+            tuesArrive.setItems(times);
+            wedArrive.setItems(times);
+            thursArrive.setItems(times);
+            friArrive.setItems(times);
+            monDepart.setItems(times);
+            tuesDepart.setItems(times);
+            wedDepart.setItems(times);
+            thursDepart.setItems(times);
+            friDepart.setItems(times);
 	}
 	
-
-	
     @FXML
-    protected void handleReturnButton(ActionEvent event) {
+    private void handleLocationCombo(ActionEvent event) {
+    	PickupLocation selectedLocation = location.getSelectionModel().getSelectedItem();
+    	memberSchedule.setPickupLocation(selectedLocation);
+    	System.out.println(selectedLocation);
+    }
+   
+    @FXML
+    private void handleMonArriveCombo(ActionEvent event) {
+    	String monArriveTime = monArrive.getSelectionModel().getSelectedItem();
+    	memberSchedule.setMonArrive(monArriveTime);
+    	System.out.println(monArriveTime);
+    }
+    
+    @FXML
+    private void handleMonDepartCombo(ActionEvent event) {
+    	String monDepartTime = monDepart.getSelectionModel().getSelectedItem();
+    	memberSchedule.setMonDepart(monDepartTime);
+    }
+    
+    @FXML
+    private void handleTuesArriveCombo(ActionEvent event) {
+    	String tuesArriveTime = tuesArrive.getSelectionModel().getSelectedItem();
+    	memberSchedule.setTuesArrive(tuesArriveTime);
+    }
+    
+    @FXML
+    private void handleTuesDepartCombo(ActionEvent event) {
+    	String tuesDepartTime = tuesDepart.getSelectionModel().getSelectedItem();
+    	memberSchedule.setTuesDepart(tuesDepartTime);    }
+    
+    @FXML
+    private void handleWedArriveCombo(ActionEvent event) {
+    	String wedArriveTime = wedArrive.getSelectionModel().getSelectedItem();
+    	memberSchedule.setWedArrive(wedArriveTime);
+    }
+    
+    @FXML
+    private void handleWedDepartCombo(ActionEvent event) {
+    	String wedDepartTime = wedDepart.getSelectionModel().getSelectedItem();
+    	memberSchedule.setWedDepart(wedDepartTime);
+    }
+    
+    @FXML
+    private void handleThursArriveCombo(ActionEvent event) {
+    	String thursArriveTime = thursArrive.getSelectionModel().getSelectedItem();
+    	memberSchedule.setThursArrive(thursArriveTime);
+    }
+    
+    @FXML
+    private void handleThursDepartCombo(ActionEvent event) {
+    	String thursDepartTime = thursDepart.getSelectionModel().getSelectedItem();
+    	memberSchedule.setThursDepart(thursDepartTime);
+    }
+    
+    @FXML
+    private void handleFriArriveCombo(ActionEvent event) {
+    	String friArriveTime = friArrive.getSelectionModel().getSelectedItem();
+    	memberSchedule.setFriArrive(friArriveTime);
+    }
+    
+    @FXML
+    private void handleFriDepartCombo(ActionEvent event) {
+    	String friDepartTime = friDepart.getSelectionModel().getSelectedItem();
+    	memberSchedule.setFriDepart(friDepartTime);
+    }
+     
+    @FXML
+    private void handleRadioButtons(ActionEvent event) {
+    	//TODO
+    	System.out.println("A Radio button was selected");
+    }
+    
+    @FXML
+    private void handleReturnButton(ActionEvent event) {
     	try {
             Parent root = FXMLLoader.load(getClass().getResource("/view/HomeScene.fxml"));
             Scene scene = new Scene(root);
@@ -98,75 +182,6 @@ public class MemberScheduleSceneController implements Initializable{
             Logger.getLogger(LoginSceneController.class.getName()).log(Level.SEVERE, null, ex);
         }
     	System.out.println("Return Button Pressed");
-    }
-    
-    /* After thinking about this for a little while we may be able to
-     * handle all of the dropdowns in one method, or probably better would 
-     * be to have two methods, one for arrival and one for departure,
-     * Then do if/else or case based on id.
-     */
-
-    @FXML
-    void handleLocationDrop(ActionEvent event) {
-    	PickupLocation selectedLocation = location.getSelectionModel().getSelectedItem();
-    	System.out.print("test new location box");
-    	System.out.print(selectedLocation);
-    }
-   
-    @FXML
-    protected void handleMonArriveDrop(ActionEvent event) {
-    	//TODO
-    }
-    
-    @FXML
-    protected void handleMonDepartDrop(ActionEvent event) {
-    	//TODO
-    }
-    
-    @FXML
-    protected void handleTuesArriveDrop(ActionEvent event) {
-    	//TODO
-    }
-    
-    @FXML
-    protected void handleTuesDepartDrop(ActionEvent event) {
-    	//TODO
-    }
-    
-    @FXML
-    protected void handleWedArriveDrop(ActionEvent event) {
-    	//TODO
-    }
-    
-    @FXML
-    protected void handleWedDepartDrop(ActionEvent event) {
-    	//TODO
-    }
-    
-    @FXML
-    protected void handleThursArriveDrop(ActionEvent event) {
-    	//TODO
-    }
-    
-    @FXML
-    protected void handleThursDepartDrop(ActionEvent event) {
-    	//TODO
-    }
-    
-    @FXML
-    protected void handleFriArriveDrop(ActionEvent event) {
-    	//TODO
-    }
-    
-    @FXML
-    protected void handleFriDepartDrop(ActionEvent event) {
-    	//TODO
-    }
-    
-    @FXML
-    protected void handleRadioButtons(ActionEvent event) {
-    	//TODO
-    	System.out.println("A Radio button was selected");
     }
 
 }
