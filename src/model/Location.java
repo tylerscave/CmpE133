5b851db2774669fae436a0749c7a51f8159776e5
tylerscave <tylerjones365@gmail.com>
2016-03-04 17:56:33 -0800

    toString for ComboBoxes

--- src/model/Location.java
+++ src/model/Location.java
@@ -1,9 +1,10 @@
 package model;
-
 /**
- *
- * @author David
- */
+ *COPYRIGHT (C) 2016 CmpE133_7. All Rights Reserved.
+ * The model for the member schedule
+ * Solves CmpE133 Assignment 2
+ * @author David Lerner, Tyler Jones,
+*/
 public class Location {
     private String name;
     
@@ -13,6 +14,11 @@ public class Location {
 
     public String getName() {
         return name;
+    }
+    // toString for ComboBoxes
+    @Override
+    public String toString() {
+    	return this.name;
     }
     
 }
