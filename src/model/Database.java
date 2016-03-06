0331b657fd9419aadf0c8734c643e6ff83f4eba2
Shubaan <shubaan_taheri@yahoo.com>
2016-03-06 09:41:29 -0800

    Database improvements

--- src/model/Database.java
+++ src/model/Database.java
@@ -1,4 +1,5 @@
 package model;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -7,6 +8,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Hashtable;
 import java.util.Scanner;
+
 /**
 *
 * Follows Singleton design pattern. Used to hold user information.
@@ -15,36 +17,37 @@ import java.util.Scanner;
 
 public class Database {
 
-		private Scanner scanner;
-		//private Hashtable <LoginInformation, Member> database;
 		private static String[][] database = new String[10][8];
-
-		public void load() throws FileNotFoundException{
-			scanner = new Scanner(new FileReader("Database"));
-			store(scanner);
-			
-		}
 		
-		public void store(Scanner scanner){
-			Context c = Context.getInstance();
-			Member member = c.getMember();
-			LoginInformation loginInfo = member.getLoginInfo();
-			/*for (int i = 0; i<10;i++){
-				for(int j = 0; j<8;j++){
-					database[i][j] = "";
+		public void load() throws FileNotFoundException{
+			Scanner countElementScanner = new Scanner(new FileReader("Database"));
+			int numOfElements = 0;
+			countElementScanner.useDelimiter(",");
+			while(countElementScanner.hasNext()){
+				System.out.print(countElementScanner.next());
+				 numOfElements++;}
+			System.out.println(numOfElements);	
+			int columns = 8;
+			int rows = numOfElements/columns;
+			BufferedReader br = new BufferedReader(new FileReader("Database"));
+			try {
+				for (int i=0;i<rows;i++){
+					String splitArray[]=br.readLine().split(",");
+					for (int j=0; j<columns;j++){
+
+						database[i][j]=splitArray[j].trim();
 					}
-				}*/
-			//while(scanner.hasNext()){
-			for (int i = 0; i<2;i++){
-				for(int j = 0; j<8;j++){
-					database[i][j] = scanner.next();//}
 					}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			for (int i=0;i<rows;i++){
+				for (int j=0; j<columns;j++){
+					System.out.print(database[i][j]+ " ");
 				}
-			for (int i = 0; i<10;i++){
-				for (int j=0;j<8;j++){
-					System.out.print(database[i][j]+" ");
-					}
-					System.out.print("\n");
+				System.out.print("\n");
+
 				}
 			try {
 				save();
@@ -52,23 +55,27 @@ public class Database {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			//database.put(loginInfo,member);
-			/*loginInfo = database.get(member);
-			String email = loginInfo.getEmail();
-			String password = loginInfo.getPassword();
-			System.out.print(email);
-			 */
-		}
+			}
+		
+
 		public static void save() throws IOException{
 		    File outFile = new File ("Database");
 		    FileWriter fWriter = new FileWriter (outFile);
 		    PrintWriter pWriter = new PrintWriter (fWriter);
-		    for (int i = 0; i<10;i++){
+		    for (int i = 0; i<7;i++){
 				for (int j=0;j<8;j++){
-					pWriter.print(database[i][j]+" ");
+					System.out.print(database[i][j]+", ");
+					}
+				System.out.print("\n");
+				}
+		    for (int i = 0; i<7;i++){
+				for (int j=0;j<8;j++){
+					pWriter.print(database[i][j]+", ");
 					}
 				pWriter.print("\n");
 				}
+			pWriter.print("saved ");
+
 		    pWriter.close();
 		}
 		public static void add(String email,String password, String lastname, String firstname, String address, String phone, MemberType memberType, String paymentMethod){
