a502646993ed5c35c81345c87a99595f446c7d81
Tyler Jones <tylerjones365@gmail.com>
2016-03-19 18:24:01 -0700

    added everything needed to support the VehicleInfoScene

--- src/model/Vehicle.java
+++ src/model/Vehicle.java
@@ -2,17 +2,67 @@ package model;
 /**
  *COPYRIGHT (C) 2016 CmpE133_7. All Rights Reserved.
  * The model for the vehicle objects.  
- * Solves CmpE133 Assignment 2
- * @author 
+ * Solves CmpE133 Carpool System
+ * @author Tyler Jones
 */
 
 public class Vehicle {
+	
+    public enum VehicleStyle {
+        Sedan,
+        TwoDoor,
+        Wagon,
+        SUV,
+        Van,
+        Truck;
+    }
+    private int year;
+    private String make;
+    private String model;
+    private String color;
 	private String plateNumber;
+	private VehicleStyle style;
 	private int capacity;
 	
-	public Vehicle(String plateNumber, int capacity){
+	public Vehicle(int year, String make, String model, String color,String plateNumber, VehicleStyle style, int capacity){
+		this.year = year;
+		this.make = make;
+		this.model = model;
+		this.color = color;
 		this.plateNumber = plateNumber;
+		this.style = style;
 		this.capacity = capacity;
+	}
+	public int getYear() {
+		return year;
+	}
+	
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	public String getMake() {
+		return make;
+	}
+	
+	public void setMake(String make) {
+		this.make = make;
+	}
+	
+	public String getModel() {
+		return model;
+	}
+	
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	
+	public void setColor(String color) {
+		this.color = color;
 	}
 	
 	public String getPlateNumber() {
@@ -22,6 +72,14 @@ public class Vehicle {
 	public void setPlateNumber(String plateNumber) {
 		this.plateNumber = plateNumber;
 	}
+	
+	public VehicleStyle getStyle() {
+		return style;
+	}
+	
+	public void setStyle(VehicleStyle style) {
+		this.style = style;
+	}
 
 	public int getCapacity() {
 		return capacity;
@@ -29,6 +87,5 @@ public class Vehicle {
 
 	public void setCapacity(int capacity) {
 		this.capacity = capacity;
-	}
-	
+	}	
 }
